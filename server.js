e47e6b13e3605652b80692c2add8e362620d12f0
Aditya Yadav <adi.2002.gwl@gmail.com>
2020-10-30 19:51:23 +0530

    fix

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ io.on('connection', socket => {
 
     // listen for chatMessage
     socket.on('chatMessage', (msg) => {
-        console.log(msg);
+        // console.log(msg);
         const  user = getCurrentUser(socket.id);
         io.to(user.room).emit('message', formatMessage(user.username, msg));
     });
